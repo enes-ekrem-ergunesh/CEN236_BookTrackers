2eb0375c466832782aaf5f05385372b1201521e6
enesekremergunesh <101215118+enesekremergunesh@users.noreply.github.com>
2022-03-16 10:18:36 +0100

    test002

--- op.php
+++ op.php
@@ -1,4 +1,13 @@
 <?php
+
+require 'vendor/autoload.php';
+
+Flight::route('/', function(){
+  echo 'Hello World!';
+});
+
+Flight::start();
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);

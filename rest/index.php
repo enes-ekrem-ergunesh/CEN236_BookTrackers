22b437af7831aee74fdc4299de434a38bcebf997
enesekremergunesh <101215118+enesekremergunesh@users.noreply.github.com>
2022-03-28 10:30:07 +0200

    successfull install of flightphp

--- rest/index.php
+++ rest/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require '../vendor/autoload.php';
+require "../vendor/autoload.php";
 
 Flight::route('/', function(){
     echo 'hello world!';

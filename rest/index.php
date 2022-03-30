6e095ecbdf8dc37e7b05689e2dc76b45c0589088
enesekremergunesh <101215118+enesekremergunesh@users.noreply.github.com>
2022-03-30 10:19:24 +0200

    Debugging 0

--- rest/index.php
+++ rest/index.php
@@ -4,11 +4,25 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require "../vendor/autoload.php";
+require_once '../vendor/autoload.php';
 
-Flight::route('/', function(){
-    echo 'hello world!';
+
+Flight::route('GET /todos', function(){
+  echo 'testttstsj';
 });
+
+//
+// Flight::route('GET /items', function(){
+//     echo 'hello test!';
+// });
+
+Flight::route('/@name', function($name){
+    echo 'require ', $name;
+});
+
+// Flight::route('/indtestex.php', function(){
+//     echo 'hello index.php!';
+// });
 
 Flight::start();
 

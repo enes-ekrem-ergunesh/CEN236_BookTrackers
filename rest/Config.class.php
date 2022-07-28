2bb5c7f5c76b584e780bc714779e46d5e709ff7d
enesekremergunesh <enes.ekrem.ergunes@stu.ibu.edu.ba>
2022-07-28 14:58:55 +0200

    db debugging

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -3,19 +3,19 @@
 class Config {
 
   public static function DB_HOST(){
-    return Config::get_env("DB_HOST", "booktrackers-db-mysql-dig-oce-fra1-85582-do-user-11842247-0.b.db.ondigitalocean.com");
+    return Config::get_env("DB_HOST", "localhost");
   }
   public static function DB_USERNAME(){
-    return Config::get_env("DB_USERNAME", "doadmin");
+    return Config::get_env("DB_USERNAME", "root");
   }
   public static function DB_PASSWORD(){
-    return Config::get_env("DB_PASSWORD", "AVNS_Peh18kw8jYC7EPydhrK");
+    return Config::get_env("DB_PASSWORD", "memduh2PRD");
   }
   public static function DB_SCHEME(){
     return Config::get_env("DB_SCHEME", "defaultdb");
   }
   public static function DB_PORT(){
-    return Config::get_env("DB_PORT", "25060");
+    return Config::get_env("DB_PORT", "3306");
   }
   public static function JWT_SECRET(){
     return Config::get_env("JWT_SECRET", "ezcb9s8UcF");
